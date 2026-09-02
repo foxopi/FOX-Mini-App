3e010dfacd4e446ec96e35eb1bb9a29d79f0be42
foxopi <foxopi0001@gmail.com>
2026-09-02 18:43:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,156 +1,37 @@
 let coin = 0;
-let dollar = 0;
 
-let invite = 0;
-
-
-/* TAP COIN */
 
 function tapCoin(){
 
-    coin += 1;
-
-    updateBalance();
-
-}
-
-
-
-/* COIN TO DOLLAR */
-
-function updateBalance(){
-
-    dollar = (coin / 100000) * 20;
-
+    coin++;
 
     document.getElementById("coinBalance").innerHTML = coin;
 
 
+    let dollar = (coin / 100000) * 20;
+
     document.getElementById("dollarBalance").innerHTML =
     "$" + dollar.toFixed(2);
-
 
 }
 
 
 
 
-
-/* PAGE CHANGE */
-
-
-function openPage(page){
+function openPage(pageID){
 
 
     let pages = document.querySelectorAll(".page");
 
 
-    pages.forEach(function(item){
+    pages.forEach(function(page){
 
-        item.classList.remove("active");
+        page.classList.remove("active");
 
     });
 
 
-    document.getElementById(page)
-    .classList.add("active");
+    document.getElementById(pageID).classList.add("active");
 
 
 }
-
-
-
-
-
-/* INVITE SYSTEM */
-
-
-function inviteFriend(){
-
-
-    if(invite < 10){
-
-        invite++;
-
-    }
-
-
-    let percent = invite * 10;
-
-
-    document.getElementById("progressBar")
-    .style.width = percent + "%";
-
-
-    document.getElementById("progressText")
-    .innerHTML = percent + "%";
-
-
-}
-
-
-
-
-
-
-/* RANK TIMER */
-
-
-let seconds = 60;
-let minutes = 60;
-let hours = 24;
-let days = 50;
-
-
-
-setInterval(function(){
-
-
-    seconds--;
-
-
-    if(seconds <=0){
-
-        seconds = 60;
-        minutes--;
-
-    }
-
-
-    if(minutes <=0){
-
-        minutes =60;
-        hours--;
-
-    }
-
-
-    if(hours <=0){
-
-        hours=24;
-        days--;
-
-    }
-
-
-
-    let timer =
-    days+" : "+
-    hours+" : "+
-    minutes+" : "+
-    seconds;
-
-
-
-    let timerBox=document.querySelector(".timer");
-
-
-    if(timerBox){
-
-        timerBox.innerHTML=timer;
-
-    }
-
-
-
-},1000);
